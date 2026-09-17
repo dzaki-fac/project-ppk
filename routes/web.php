bb381d6b1705e692f4afdfe744fa422e294a5939
galviyy <galvindoser@gmail.com>
2026-09-17 11:37:07 +0700

    feat(auth): perketat validasi registrasi/login/user-crud + hash + session + test keamanan

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ Route::get('/', function () {
 
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:10,1');
     Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/register', [AuthController::class, 'register'])->middleware('throttle:10,1');
 });
 
 Route::middleware('auth')->group(function () {
